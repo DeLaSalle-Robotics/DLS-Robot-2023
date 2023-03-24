3d896d6235f74fcada605eea766584c770e22015
DeWayne Townsend <town0045@umn.edu>
2023-03-24 14:00:26 -0500

    Remove redundant arm position portion of the command

--- src/main/java/frc/robot/commands/CubePickUp.java
+++ src/main/java/frc/robot/commands/CubePickUp.java
@@ -14,12 +14,11 @@ public class CubePickUp extends SequentialCommandGroup{
     private DrivetrainSubsystem m_drive;
     private Intake m_intake;
 
-    public CubePickUp(DrivetrainSubsystem _drive, Intake _intake, Arm _arm, ArmExtend _armExtend){
+    public CubePickUp(DrivetrainSubsystem _drive, Intake _intake){
         m_drive = _drive;
         m_intake = _intake;
 
         addCommands(
-            new ArmPlaceCommand(230, .35, _arm, _armExtend),
             new ParallelCommandGroup(
                 new HuntingCubes(0, m_drive),
                 new SpinIntake(m_intake, ()->Constants.IntakeSpeed, ()->0)
